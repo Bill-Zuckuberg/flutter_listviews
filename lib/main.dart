39f73360ca0e2104a15d4a59ddc20e87be8d36ff
Bill-Zuckuberg <thanh821329@gmail.com>
2021-10-28 22:21:49 +0700

    disable debugBaner

--- lib/main.dart
+++ lib/main.dart
@@ -12,6 +12,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowMaterialGrid: false,
       title: 'Demo ListViews',
       home: RamdomWords(),
       debugShowCheckedModeBanner: false,
